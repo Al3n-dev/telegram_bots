3b8de588bd89ca0f74deabcae6834b722901546f
Alen Schrödinger <98950513+Al3n-dev@users.noreply.github.com>
2022-02-11 22:40:36 +0300

    Add files via upload

---
+++ bones.py
@@ -0,0 +1,32 @@
+from aiogram import Bot, Dispatcher, types, executor
+from asyncio import sleep
+
+
+bot = Bot('5132582034:AAHBkN4putW87yoq-ocu9l-c4DVawInH84w')
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def on_message(message: types.Message):
+    await bot.send_message(message.from_user.id, f"Привет {message.from_user.username}! Начинаем игру пидр")
+    await sleep(1)
+
+    bot_data = await bot.send_dice(message.from_user.id)
+    bot_data = bot_data['dice']['value']
+    await sleep(5)
+
+
+    user_date = await bot.send_dice(message.from_user.id)
+    user_date = user_date['dice']['value']
+    await sleep(5)
+
+    if bot_data > user_date:
+        await bot.send_message(message.from_user.id, "Вы проиграли!")
+    elif bot_data < user_date:
+        await bot.send_message(message.from_user.id, "Вы победили!")
+    else:
+        await bot.send_message(message.from_user.id, "Ничья!")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
